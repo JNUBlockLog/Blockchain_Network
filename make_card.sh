8a5dc4eb267cecbf26704258e336d831c9b7832b
HardPlant <abc7988se@naver.com>
2018-05-03 09:30:42 -0700

    factory-network
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- make_card.sh
+++ make_card.sh
@@ -2,3 +2,12 @@
 
 composer archive create --archiveFile factory-network@$1.bna\
  --sourceType dir --sourceName factory-network
+
+composer network install -c PeerAdmin@fabric-network -a factory-network@$1.bna
+
+if [ $1 -eq "0.0.1"]; then
+    composer network start --networkName factory-network\
+    --networkVersion $1 -A admin -S adminpw -c PeerAdmin@fabric-network
+else
+    composer network upgrade -n factory-network\
+     -V $1 -c PeerAdmin@fabric-network
