ed2b408e0393be426ec45d2853c5a5684b547dab
HardPlant <abc7988se@naver.com>
2018-05-24 23:40:31 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

---
+++ composer_rest_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NETWORK_NAME=factory-network
+NETWORK_VERSION=0.2.10-deploy.4
+COMPOSER_CARD=admin@$NETWORK_NAME
+
+composer-rest-server -c $COMPOSER_CARD
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1 = filename
+# $2 = networkName
+NETWORK_NAME=factory-network
+NETWORK_VERSION=0.2.10-deploy.4
+PEER_ADMIN=PeerAdmin@fabric-network
+
+
+if [ $1 -eq 'upgrade' ]; then
+    composer network install -c PeerAdmin@fabric-network -a $NETWORK_NAME@$NETWORK_VERSION.bna
+    composer network start --networkName $NETWORK_NAME --networkVersion $NETWORK_VERSION -A admin -S adminpw -c $PEER_ADMIN
+else
+    composer network upgrade -c $PEER_ADMIN -n $NETWORK_NAME -V $NETWORK_VERSION
+if
