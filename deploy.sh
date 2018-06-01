47394d680efe235889c1613e4df7c88dd8730628
HardPlant <abc7988se@naver.com>
2018-05-31 23:22:23 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ NETWORK_VERSION=0.2.10-deploy.4
 PEER_ADMIN=PeerAdmin@fabric-network
 
 
-if [ $1 -eq 'upgrade' ]; then
+if [[ $1 != 'upgrade' ]]; then
     composer network install -c PeerAdmin@fabric-network -a $NETWORK_NAME@$NETWORK_VERSION.bna
     composer network start --networkName $NETWORK_NAME --networkVersion $NETWORK_VERSION -A admin -S adminpw -c $PEER_ADMIN
 else
     composer network upgrade -c $PEER_ADMIN -n $NETWORK_NAME -V $NETWORK_VERSION
-if
+fi
